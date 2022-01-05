dc37e1757492f8979432fff4ab3f082f59727324
João Pedro Lazarim de Souza <jplazarim@gmail.com>
2022-01-05 15:12:35 -0300

    Update security.go

--- example/security/security.go
+++ example/security/security.go
@@ -30,7 +30,6 @@ func VerifyWebToken(c *gin.Context) {
 	}
 
 	_, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
